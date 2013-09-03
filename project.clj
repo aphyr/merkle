e6af2af708dfc6395587c4247885326cecd1d2c5
aphyr <aphyr@aphyr.com>
2013-09-03 14:31:24 -0700

    0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject merkle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject merkle "0.1.0"
+  :description "Merkle trees for constructing efficient diffs over collections."
+  :url "http://github.com/aphyr/merkle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
