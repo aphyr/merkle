9618ac4e24c66510431506f092f2622853b3a372
Zach Tellman <ztellman@gmail.com>
2013-09-25 13:07:43 -0700

    add missing hint, and turn on reflection warnings

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [primitive-math "0.1.3"]]
+  :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/data.generators "0.1.0"]
                                   [reiddraper/simple-check "0.4.0"]]}})
--- src/merkle/kv/fixed.clj
+++ src/merkle/kv/fixed.clj
@@ -78,7 +78,7 @@
             (let [^CRC32 c (get-crc 0)]
               (.update c x)
               (when emit-all?
-                (.add (aget lists 0) x))))
+                (.add ^ArrayList (aget lists 0) x))))
           
           ;; ascend the levels as appropriate
           (loop [idx idx, level 0]
